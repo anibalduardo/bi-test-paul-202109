325425858b8192064f6c49aeced9afd93c5c55a7
antonizero99 <31177929+antonizero99@users.noreply.github.com>
2021-09-30 19:17:05 +0700

    Update Dummy_data script and update README file

--- Dummy_data.sql
+++ Dummy_data.sql
@@ -1,15 +1,15 @@
 -- Insert dummy data
 
 TRUNCATE TABLE [dbo].[cases]
-INSERT INTO [dbo].[cases] VALUES (1, 1,'Ty01','2021-8-30 09:30:00', '2021-9-1 09:30:00', 'Cap', 'Avenger')
-INSERT INTO [dbo].[cases] VALUES (2, 1,'Ty01','2021-8-30 09:30:00', '2021-9-2 09:30:00', 'Cap', 'Avenger')
-INSERT INTO [dbo].[cases] VALUES (3, 2,'Ty01','2021-8-30 09:30:00', '2021-9-1 09:30:00', 'Buck', 'Avenger')
-INSERT INTO [dbo].[cases] VALUES (4, 3,'Ty01','2021-8-30 09:30:00', '2021-9-1 09:30:00', 'Buck', 'Avenger')
-INSERT INTO [dbo].[cases] VALUES (5, 4,'Ty01','2021-8-30 09:30:00', '2021-9-2 09:30:00', 'Hulk', 'Avenger')
-INSERT INTO [dbo].[cases] VALUES (6, 4,'Ty01','2021-8-30 09:30:00', '2021-9-4 09:30:00', 'Hulk', 'Avenger')
-INSERT INTO [dbo].[cases] VALUES (7, 5,'Ty01','2021-8-30 09:30:00', '2021-9-2 09:30:00', 'Hulk', 'Avenger')
+INSERT INTO [dbo].[cases] VALUES (1, 1,'Open','2021-8-30 09:30:00', '2021-9-1 09:30:00', 'Cap', 'Avenger')
+INSERT INTO [dbo].[cases] VALUES (2, 1,'Open','2021-8-30 09:30:00', '2021-9-2 09:30:00', 'Cap', 'Avenger')
+INSERT INTO [dbo].[cases] VALUES (3, 2,'Open','2021-8-30 09:30:00', '2021-9-1 09:30:00', 'Buck', 'Avenger')
+INSERT INTO [dbo].[cases] VALUES (4, 3,'Closed','2021-8-30 09:30:00', '2021-9-1 09:30:00', 'Buck', 'Avenger')
+INSERT INTO [dbo].[cases] VALUES (5, 4,'Closed','2021-8-30 09:30:00', '2021-9-2 09:30:00', 'Hulk', 'Avenger')
+INSERT INTO [dbo].[cases] VALUES (6, 4,'Closed','2021-8-30 09:30:00', '2021-9-4 09:30:00', 'Hulk', 'Avenger')
+INSERT INTO [dbo].[cases] VALUES (7, 5,'Closed','2021-8-30 09:30:00', '2021-9-2 09:30:00', 'Hulk', 'Avenger')
 
-
+TRUNCATE TABLE [dbo].[csat]
 INSERT INTO [dbo].[csat] VALUES (1, 1,5,'2021-8-30 09:30:00')
 INSERT INTO [dbo].[csat] VALUES (2, 2,NULL,'2021-8-30 09:30:00')
 INSERT INTO [dbo].[csat] VALUES (3, 3,4,'2021-8-30 09:30:00')
@@ -18,9 +18,11 @@ INSERT INTO [dbo].[csat] VALUES (5, 5,3,'2021-8-30 09:30:00')
 INSERT INTO [dbo].[csat] VALUES (6, 6,5,'2021-8-30 09:30:00')
 INSERT INTO [dbo].[csat] VALUES (7, 7,NULL,'2021-8-30 09:30:00')
 
+TRUNCATE TABLE [dbo].[owner]
 INSERT INTO [dbo].[owner] VALUES (1, 'Sheild')
 INSERT INTO [dbo].[owner] VALUES (2, 'Uncle Ben')
 
+TRUNCATE TABLE [dbo].[properties]
 INSERT INTO [dbo].[properties] VALUES (1, '123 stre', 'New York', 'USA', 1)
 INSERT INTO [dbo].[properties] VALUES (2, '123 stre', 'New York', 'USA', 1)
 INSERT INTO [dbo].[properties] VALUES (3, '123 stre', 'New York', 'USA', 1)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bi-test
 test bi developer
+
+
+ANALYTICS WORKING PROCEDURE
+1. Understanding data schema
+2. Generate dummy data for analytics
+3. Build a database to store staging data and views for analytics
+4. Load data into Power BI for building dashboards
